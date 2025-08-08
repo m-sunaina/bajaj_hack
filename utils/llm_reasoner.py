45cdcaf2bb5f317933b7f80b6a2b0f15f1a3bd6a
m-sunaina <manyamalas.cs23@rvce.edu.in>
2025-08-08 22:15:27 +0530

    Update llm_reasoner.py

--- utils/llm_reasoner.py
+++ utils/llm_reasoner.py
@@ -19,7 +19,7 @@ QDRANT_COLLECTION = "insurance_docs"
 # ======================
 # GEMINI CONFIG
 # ======================
-GEMINI_API_KEY = "AIzaSyBiSObq25v6xWEztJOG3HxyQo1sCf_ee3U"
+GEMINI_API_KEY = os.getenv(("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
